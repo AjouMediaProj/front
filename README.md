6dc640523eeb44d54e8c07348e2921c1891f64a0
kahmnkk <kyuhh1214@naver.com>
2021-10-17 03:19:15 +0900

    add. SSR (Server Side Rendering)
    - server.js
    - usage in README

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # Front
 
 Vote React App
+
+## Usage
+
+Develop Mode
+
+```
+yarn start
+```
+
+Production Mode
+
+```
+yarn build
+node server
+```
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+function main() {
+    app.use(express.json());
+    app.use('/', express.static(__dirname + '/build'));
+
+    app.get('*', (req, res, next) => {
+        if (req.path.split('/')[1] === 'static') return next();
+
+        res.sendFile(`${__dirname}/build/index.html`);
+    });
+
+    app.listen(PORT, () => {
+        console.log(`run node server port: ${PORT}`);
+    });
+}
+main();
