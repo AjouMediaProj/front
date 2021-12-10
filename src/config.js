fca48af34362294c068cb656277f9b31ce8072a6
kahmnkk <kyuhh1214@naver.com>
2021-12-11 01:22:09 +0900

    add. config apiURL
    - add aws url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 const config = {
-    //apiURL: 'http://211.243.253.95:8080',
-    apiURL: 'http://59.12.7.180:8080',
+    // apiURL: 'http://211.243.253.95:8080',
+    // apiURL: 'http://59.12.7.180:8080',
+    apiURL: 'http://3.36.114.210:8080',
 };
 
 module.exports = config;
