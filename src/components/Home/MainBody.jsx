a2b32d00e68957d97273f44e18fc9c1ca228a5c2
kahmnkk <kyuhh1214@naver.com>
2021-12-23 18:49:18 +0900

    fix. main page form

--- src/components/Home/MainBody.jsx
+++ src/components/Home/MainBody.jsx
@@ -16,33 +16,34 @@ const StyledBody = styled.div`
     background-size: 100% auto;
 
     h1 {
-        margin-top: 10vh;
+        margin-top: 14vh;
         margin-right: 10%;
         color: white;
-        font-size: 50px;
+        font-size: 45px;
     }
     h2 {
-        margin-top: 1vh;
         margin-right: 10%;
         color: white;
-        font-size: 50px;
+        font-size: 45px;
     }
     h3 {
-        margin-top: 3vh;
+        margin-top: 2vh;
         margin-right: 10%;
         color: white;
-        font-size: 40px;
+        font-size: 31px;
     }
 `;
 
 const LinkBtn = styled.button`
-    width: 30vw;
+    width: 25vw;
     height: 7vh;
     border: 1px solid white;
-    border-radius: 10px;
+    border-radius: 50px;
     background-color: rgba(0, 0, 0, 0);
     color: white;
-    font-size: 30px;
+    margin-top: 7vh;
+    margin-right: 10%;
+    font-size: 24px;
     :hover {
         background-color: white;
         color: black;
@@ -53,13 +54,11 @@ class MainBody extends Component {
     render() {
         return (
             <StyledBody>
-                <h1>BlockChain</h1>
+                <h1>Blockchain</h1>
                 <h2>Vote</h2>
                 <h2>System</h2>
                 <h3>보다 투명하게, 보다 공정하게</h3>
-                <h3>
-                    <LinkBtn onClick={() => (window.location.href = '/vote')}>아주대학교 학생회 투표 참여하기</LinkBtn>
-                </h3>
+                <LinkBtn onClick={() => (window.location.href = '/vote')}>아주대학교 학생회 투표 참여하기</LinkBtn>
             </StyledBody>
         );
     }
